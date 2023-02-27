f460e7f48c33f6619d7c6a9a62262c6cca8b1680
221RDB222 <124578710+MartinsBoikovs@users.noreply.github.com>
2023-02-28 01:51:47 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -1,58 +1,51 @@
-# python3
+#Martins Boikovs 4.grupa
+#rakstiju Codespace, bet fiksacija man neizdavas nekā, vienkarši  bezgalīga ieladēšanā, tapēc to nokopēju no codespace un ievietoju šajā failā
 
 import sys
 import threading
-import numpy as np
+#import numpy as np
 
-class Node:
+class Node: #klase lai katram elementam butu pieskirta vina heigth lidz galvenei
     def __init__(self, n):
         self.n = n
         self.children = []
         self.height = None
 
 def compute_height(n, parents):
-    
+    nodes = [Node(i) for i in range(n)] #lists ar Node tipa mainigiem
+    root = None
 
+    for i, p in enumerate(parents): #eju cauri visiem elementiem kokā lai atrastu galveni un visus bernus
+        if p == -1:
+            root = nodes[i]
+        else:
+            nodes[p].children.append(nodes[i])
 
-    #max_height = 0
+    if root is None:
+        return 0
 
-    #rewrite from list to numpy array !!!, tests 21-25???
+    storage = [(root, 1)] #storage ar elementa numuram un augstumu no ta lidz galvenei
+    max_height = 0
+    while storage:
+        node, height = storage.pop()
+        node.height = height
+        max_height = max(max_height, height)
+        for child in node.children:
+            storage.append((child, height + 1))
 
-    #par_split = parents.split()
-    #par_split = map(int, par_split)
-    #par_split = list(par_split)
-    #ar_split = np.array(par_split)
-    #print(par_split)
-
-    #already_was = {}
-
-    #for i in range(n):
-     #   if i not in already_was:
-      #      current_h = 0
-       #     curr = i
-        #    while curr > -1:
-         #       if curr in already_was:
-          #          current_h += already_was[curr]
-           #         break
-            #    current_h += 1
-             #   curr = par_split[curr]
-              #  already_was[i] = current_h
-        #max_height = max(max_height, already_was[i])
-                
+    return max_height
 
 def main():
     text = input()
     if 'F' in text and not 'a' in text:
         file_name = input()
         file = "./test/" + file_name
-        #print(file)
         with open(file) as f:
             num = f.readline()
             text = f.readline()
     if 'I' in text:
         num = input()
         text = input()
-    
     print(compute_height(int(num), list(map(int, text.split()))))
 
 sys.setrecursionlimit(10**7)  
